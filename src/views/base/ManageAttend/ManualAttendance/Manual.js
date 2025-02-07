355fdbdf79cd49812c896c730f08455b6ece3ad5
anirudhhbehera <anirudhhbehera@gmail.com>
2025-02-07 16:36:26 +0530

    fixed

--- src/views/base/ManageAttend/ManualAttendance/Manual.js
+++ src/views/base/ManageAttend/ManualAttendance/Manual.js
@@ -320,7 +320,7 @@ import { COLUMNS } from './columns'
 import { StyledTablePagination } from '../../../../views/PaginationCssFile/TablePaginationStyles'
 // import { FaBriefcase   } from 'react-icons/fa';
 import { BsBuildingsFill } from 'react-icons/bs'
-import '../../ReusablecodeforTable/style.css'
+import '../../ReusablecodeforTable/styles.css'
 import ExcelJS from 'exceljs';
 import PDFExporter from '../../ReusablecodeforTable/PDFExporter'
 import ExcelExporter from '../../ReusablecodeforTable/ExcelExporter'
